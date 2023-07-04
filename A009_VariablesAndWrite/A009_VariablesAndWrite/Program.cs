c3e96f968b75d3bf1a247cdae037b75a0c4f09d2
ekdlakdl12 <rkfflffpdh12@gmail.com>
2023-07-04 18:58:09 +0900

    코드추가

--- A009_VariablesAndWrite/A009_VariablesAndWrite/Program.cs
+++ A009_VariablesAndWrite/A009_VariablesAndWrite/Program.cs
@@ -10,6 +10,16 @@ namespace A009_VariablesAndWrite
     {
         static void Main(string[] args)
         {
+            int v1 = 100;
+            double v2 = 1.234;
+
+            //Console.WriteLine(v1,v2); 에러가 발생함
+            Console.WriteLine(v1.ToString() + ", " + v2.ToString()); //숫자를 문자로 변환
+            Console.WriteLine("v1 = " + v1 + ", v2 = " + v2);
+            Console.WriteLine("v1 = {0} , v2 = {1}", v1, v2); 
+            Console.WriteLine($"v1 = {v1}, v2 = {v2}"); // 파이썬에서는 볼수없던 방법
+
+
         }
     }
 }
