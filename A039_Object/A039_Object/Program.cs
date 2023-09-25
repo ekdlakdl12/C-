ab848ee1acfd491ab3dfd35d7ad1c704f78d2687
JunYoung Choi <56615819+ekdlakdl12@users.noreply.github.com>
2023-09-25 17:25:09 +0900

    39 add
    39 add

---
+++ A039_Object/A039_Object/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A039_Object
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 123;
+            object o = i; //i값을 박싱하여 o로 복사함
+            i = i + 10; //i값을 바꿉니다 이미 박싱해둔 o값은 변하지 않음
+            int j = (int)o; // o값을 언박싱하여 j에 복사
+
+            Console.WriteLine("The value - type value i = {0}", i);
+            Console.WriteLine("The object - type value o =  {0}", o);
+            Console.WriteLine("The value - type value j = {0}", j);
+
+            object p = o;// p에 박싱
+            o = 100;
+            Console.WriteLine("The object - type value o = {0}", o);
+            Console.WriteLine("The object - type value p = {0}", p);
+
+        }
+    }
+}
