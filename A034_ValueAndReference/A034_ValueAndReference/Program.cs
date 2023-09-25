8d2d4377598e99fb8e0a416326a95dfe51e79165
JunYoung Choi <56615819+ekdlakdl12@users.noreply.github.com>
2023-09-25 16:17:06 +0900

    34
    34

---
+++ A034_ValueAndReference/A034_ValueAndReference/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A034_ValueAndReference
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "before passing";
+            Console.WriteLine(s); //전처리
+
+            Test(s);
+            Console.WriteLine(s); //그냥 출력 전처리
+
+            Test(ref s);
+            Console.WriteLine(s); //Ref로 인한 후처리
+        }
+
+        public static void Test(String s)
+        {
+            s = "after passing";
+        }
+
+        public static void Test(ref String s)
+        {
+            s = "after passing";
+        }
+    }
+}
