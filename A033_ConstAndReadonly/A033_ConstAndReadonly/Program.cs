17b462bd15a6cb65208b68d556e1c874ebc20a40
JunYoung Choi <56615819+ekdlakdl12@users.noreply.github.com>
2023-09-25 16:16:43 +0900

    33
    33

---
+++ A033_ConstAndReadonly/A033_ConstAndReadonly/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A033_ConstAndReadonly
+{
+    class ConstEx
+    {
+        public const int number = 3;
+    }
+
+    class ReadonlyEx
+    {
+        public readonly int number = 10;
+        public ReadonlyEx()
+        {
+            number = 20;
+        }
+        public ReadonlyEx(int n)
+        {
+            number = n;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ConstEx.number); //Const사용 / 값 변경 불가능
+
+            ReadonlyEx inst1 = new ReadonlyEx(); //Readonly 사용 / 처음입력되잇던값 20출력 
+            Console.WriteLine(inst1.number);
+
+            ReadonlyEx inst2 = new ReadonlyEx(100); //Readonly 사용 /변경 200출력
+            Console.WriteLine(inst2.number);
+        }
+    }
+}
