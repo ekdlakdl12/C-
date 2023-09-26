881783249a05a469fb5a145ea6dcd53b3ca9d565
JunYoung Choi <56615819+ekdlakdl12@users.noreply.github.com>
2023-09-26 21:32:37 +0900

    042
    042

--- A042_BMI/A042_BMI/Program.cs
+++ A042_BMI/A042_BMI/Program.cs
@@ -10,6 +10,29 @@ namespace A042_BMI
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("키를 입력하세요 (cm): ");
+            double height = double.Parse(Console.ReadLine());
+            
+            height = 100; //m단위
+
+            Console.WriteLine("체중을 입력하세요");
+            double weight = double.Parse(Console.ReadLine());
+
+            double bmi = weight / (height*height);
+
+            string comment = null;
+
+            if (bmi < 20)
+                comment = "저체중";
+            else if (bmi < 25)
+                comment = "정상체중";
+            else if (bmi < 30)
+                comment = "경도비만";
+            else if (bmi < 40)
+                comment = "고도비만";
+
+            Console.WriteLine("BMI={0:F1}, \"{1}\"입니다",bmi,comment);
+            //{0:F1} 소숫점 첫째자리 이후 반올림 하라는 뜻
         }
     }
 }
