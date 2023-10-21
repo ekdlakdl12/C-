c546978ecb1063e726c14fedea0bdd75169ea339
JunYeong Choi <56615819+ekdlakdl12@users.noreply.github.com>
2023-10-21 18:19:00 +0900

    A054
    A054

---
+++ A054_TimeTable/A054_TimeTable/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A054_TimeTable
+{
+    internal class Program
+    {
+        static void Main(string[] args) //gugudan
+        {
+            for(int y = 1; y <= 9;  y++) // 1,2,3,4,5,6,7,8,9
+            {
+                Console.WriteLine();
+                for (int x = 2; x <= 9; x++) //2.3.4.5.6.7.8.9
+                    Console.WriteLine("{0} x {1} = {2,2}", y, x, y * x); // 2 * 1
+                Console.WriteLine();
+            }   
+        }
+    }
+}
