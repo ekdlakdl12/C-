13d6fbcbdf118c7a32f92af8dc7d6bd6368a2837
JunYeong Choi <56615819+ekdlakdl12@users.noreply.github.com>
2023-10-30 18:27:12 +0900

    Add files via upload

---
+++ A070_StaticMethod/A070_StaticMethod/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A070_StaticMethod
+{
+    class Methods
+    { 
+        static void Main(string[] args)
+        {
+            int a = 10, b = 30, c = 20;
+            Methods x = new Methods();
+            Console.WriteLine("가장 큰 수는 {0}", x.Larger(x.Larger(a,b),c));
+
+        }
+
+        private int Larger(int a,int b) 
+        {
+            return (a >= b) ? a : b; //3항
+        }
+
+    }
+}
