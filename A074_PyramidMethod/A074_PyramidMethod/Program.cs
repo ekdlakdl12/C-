02543c7fe2514abd1d7de32225777f94db54aceb
JunYeong Choi <56615819+ekdlakdl12@users.noreply.github.com>
2023-11-02 18:33:58 +0900

    Add files via upload

---
+++ A074_PyramidMethod/A074_PyramidMethod/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A074_PyramidMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DrawPyramid(3);
+            DrawPyramid(5);
+            DrawPyramid(7);
+        }
+
+        static void DrawPyramid(int n)
+        {
+            for (int i = 1;  i <= n; i++)                             // i= 1부터 3까지 반복
+            {                                                         // j= 1부터 3까지 반복
+                for (int j = i; j < n; j++)                           // k= 1부터 2 * 1 - 1 k++    1개 3개 5개 찍힘
+                    Console.Write(" ");
+                for (int k = 1; k <= 2 * i - 1; k++)
+                    Console.Write("*");
+                Console.WriteLine();
+                
+            }
+        }
+    }
+}
