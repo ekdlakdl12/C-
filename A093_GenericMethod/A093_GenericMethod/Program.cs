eb343ded4d70933de4b9d8573533c9239a475dc3
JunYeong Choi <56615819+ekdlakdl12@users.noreply.github.com>
2023-11-13 21:37:53 +0900

    Add files via upload

---
+++ A093_GenericMethod/A093_GenericMethod/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//일반화 메소드 데이터 유형에 관계없이 사용가능
+namespace A093_GenericMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 1, 2, 3 };
+            double[] d = { 0.1, 0.2, 0.3 };
+            string[] s = { "tiger", "lion", "zebra" };
+
+            PrintArray<int>(a);
+            PrintArray<double>(d);
+            PrintArray<string>(s);
+        }
+
+        private static void PrintArray<T>(T[] a) //T(Type) 제네릭 
+        {
+            foreach(var item in a)
+                Console.WriteLine("{0,8}",item);
+            Console.WriteLine();
+        }
+
+    }
+}
