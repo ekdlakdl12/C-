f7b6aa4fa19cd83d5fd32141c0b48189a2793bb2
JunYeong Choi <56615819+ekdlakdl12@users.noreply.github.com>
2023-11-13 17:17:01 +0900

    버그수정,드라이버문제수정
    New) 매크로 방지 문자 해결 필요

--- ExamSelenium/Form1.cs
+++ ExamSelenium/Form1.cs
@@ -14,8 +14,7 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 
-//만들었더니 크롬 드라이버 버전이랑 현재 크롬버전이 맞지않아 사용이 불가능함
-
+//자동방지프로그램 처리 못하면 사용못함
 
 namespace ExamSelenium
 {
@@ -56,7 +55,7 @@ namespace ExamSelenium
 
             if (caption == "로그인")
             {   
-                //아이디 로그인박스 지정
+          
                 string id = textBox1.Text;
                 //비밀번호 로그인 박스 지정
                 string pw = textBox2.Text;
@@ -68,22 +67,22 @@ namespace ExamSelenium
                 _driver = new ChromeDriver(_driverService, _options); //크롬관련 기능 사용을 위한 메서드
 
                 _driver.Navigate().GoToUrl("https://www.naver.com"); //웹사이트 접속
-
+               
                 _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10); // 충돌방지 
 
                 var element = _driver.FindElement(By.XPath("//*[@id='account']/div/a/i")); //찿으려는요소 html테그
                 element.Click();
 
-                Thread.Sleep(3000);
+                Thread.Sleep(3000); //3초간 기다림
 
                 element = _driver.FindElement(By.XPath("//*[@id='id']"));
-                element.SendKeys(id); //??
+                element.SendKeys(id); //Value를 input tag에 쓰는 메소드
 
                 element = _driver.FindElement(By.XPath("//*[@id='pw']"));
-                element.SendKeys(pw); //??
+                element.SendKeys(pw); 
 
-                element = _driver.FindElement(By.XPath("//*[@id='log,login']"));
-                element.Click(); //??
+                element = _driver.FindElement(By.XPath("//*[@id='log.login']"));   //*[@id="log.login"] //*[@id="log.login"]/span
+                element.Click(); 
 
                 button1.Text = "로그아웃";
             }
