198182ca5937a1f7080c81867568e7119aaee534
JunYeong Choi <56615819+ekdlakdl12@users.noreply.github.com>
2023-11-13 21:06:28 +0900

    Add files via upload

---
+++ A091_OptionalNamedArguments/A091_OptionalNamedArguments/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A091_OptionalNamedArguments
+{
+    internal class Program
+    {
+        static int MyPower(int x, int y = 2) //if문 for문 대괄호안씀
+        {
+            int result = 1;
+            for (int i = 0; i< y; i++)
+                result = result * x;
+            return result;
+ 
+        }
+ 
+        static int Area(int h , int w) 
+        {
+            return h * w;
+        }
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MyPower(4, 2));
+            Console.WriteLine(MyPower(4));
+            Console.WriteLine(MyPower(3,4));
+
+            Console.WriteLine(Area(w:5,h:6));
+            Console.WriteLine(Area(h:6,w:5));
+        
+        }
+    }
+}
