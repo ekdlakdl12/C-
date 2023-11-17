cc88118c47f1b0d9b8794d37ac64593d78029ab7
JunYeong Choi <56615819+ekdlakdl12@users.noreply.github.com>
2023-11-17 15:31:31 +0900

    A098 ~ A099

---
+++ A098_StackImplementation/A098_StackImplementation/MyStack.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+
+//Stack - Last in - First out LIFO
+
+namespace A098_StackImplementation
+{
+    class MyStack<T>
+    {
+        const int maxSize = 10;
+        private T[] arr = new T[maxSize];
+        private int top = 0;
+
+        public MyStack() 
+        {
+            top = 0;
+        }
+
+        //스택에 집어넣기
+        public void Push(T val)
+        {
+            if(top < maxSize)
+            {
+                arr[top] = val;
+                ++top;
+            }
+            else
+            {
+                Console.WriteLine("Stack Full");
+                return;
+            }
+        }
+        public T Pop()
+        {
+            if (top > 0)
+            {
+                --top;
+                return arr[top];
+            }
+            else
+            {
+                Console.WriteLine("Stack Empty");
+                return default(T);
+            }
+        }
+
+    }
+}
---
+++ A098_StackImplementation/A098_StackImplementation/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A098_StackImplementation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStack<int> stack = new MyStack<int>();
+            Random r = new Random();
+
+            for(int i = 0; i < 10; i++) 
+            {
+                int val = r.Next(100);
+                stack.Push(val);
+                Console.WriteLine("Push(" + val + ")");
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < 10; i++)
+                Console.WriteLine("Pop() = " + stack.Pop() + ",");
+        }
+    }
+}
