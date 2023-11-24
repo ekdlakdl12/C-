7261b31e3b110638a5aaf4192e7df334eaad871a
JunYeong Choi <56615819+ekdlakdl12@users.noreply.github.com>
2023-11-24 17:37:47 +0900

    Add files via upload

---
+++ A112_Indexer/A112_Indexer/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A112_Indexer
+{
+    public class Program
+    {   
+        class MyCollection<T>
+        {
+            private T[] array = new T[100];
+
+            public T this[int i] //인덱서정의 
+            {
+                get { return array[i]; }
+                set { array[i] = value; }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var myString = new MyCollection<string>();
+
+            myString[0] = "Hello string";
+            myString[1] = "Hello, C#";
+            myString[2] = "Hello, Indexxer!";
+
+            for (int i =0; i<3; i++)
+                Console.WriteLine(myString[i]);
+                
+            
+
+        }
+    }
+}
