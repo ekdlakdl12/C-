81d4dfea8de37399af96f1ae64961c7a18e5b25c
JunYeong Choi <56615819+ekdlakdl12@users.noreply.github.com>
2023-12-08 14:39:17 +0900

    Add files via upload

---
+++ A118_Predicate/A118_Predicate/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A118_Predicate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //람다식
+            Predicate<int> isEven = n => n% 2 == 0;
+            Console.WriteLine(isEven(6));
+
+            //Equals  == 와 같은뜻
+            Predicate<string> isLowerCase = s => s.Equals(s.ToLower());
+            Console.WriteLine(isLowerCase("This is Test"));
+        }
+    }
+}
